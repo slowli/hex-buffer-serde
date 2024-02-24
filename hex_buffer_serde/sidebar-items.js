945e636fe98bff855d21017384214db972a35a8b
slowli <slowli@users.noreply.github.com>
2024-02-24 08:00:12 +0000

    Deploying to gh-pages from @ slowli/hex-buffer-serde@12dfd630b913b744735c7c4b69142e632f6c235d 🚀

---
+++ hex_buffer_serde/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ConstHexForm","HexForm"],"trait":["ConstHex","Hex"]};
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"hex_buffer_serde":["",[],["const_len.rs","lib.rs","var_len.rs"]]\
+}');
+createSrcSidebar();
