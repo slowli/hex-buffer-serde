02135f387a64ee39ef8837fd2e62391bc97872fb
slowli <slowli@users.noreply.github.com>
2024-11-10 15:42:18 +0000

    Deploying to gh-pages from @ slowli/hex-buffer-serde@3f40a4fa2490abcf6321cb17f99c0c0dafd06dff 🚀

---
+++ trait.impl/core/fmt/trait.Debug.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hex_buffer_serde",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hex_buffer_serde/struct.ConstHexForm.html\" title=\"struct hex_buffer_serde::ConstHexForm\">ConstHexForm</a>&lt;T&gt;"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hex_buffer_serde/struct.HexForm.html\" title=\"struct hex_buffer_serde::HexForm\">HexForm</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[867]}
