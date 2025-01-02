b9ce2303acee3247b0292e184e4e458b8ab35ee3
slowli <slowli@users.noreply.github.com>
2025-01-02 20:36:31 +0000

    Deploying to gh-pages from @ slowli/hex-buffer-serde@cf72759b0f84305cf196b2f7ec5a479f0063f973 🚀

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["hex_buffer_serde"];
+//{"start":21,"fragment_lengths":[18]}
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["hex_buffer_serde",["",[],["const_len.rs","lib.rs","var_len.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[67]}
