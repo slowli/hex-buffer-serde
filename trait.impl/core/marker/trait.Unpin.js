cd8aaebe7b658c440a55301e245ecbd2313b645c
slowli <slowli@users.noreply.github.com>
2025-11-04 09:42:52 +0000

    Deploying to gh-pages from @ slowli/hex-buffer-serde@6cb13dee6e4f624aa122591f4e143812b115118b 🚀

---
+++ trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hex_buffer_serde",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"hex_buffer_serde/struct.ConstHexForm.html\" title=\"struct hex_buffer_serde::ConstHexForm\">ConstHexForm</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,</div>",1,["hex_buffer_serde::const_len::ConstHexForm"]],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"hex_buffer_serde/struct.HexForm.html\" title=\"struct hex_buffer_serde::HexForm\">HexForm</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,</div>",1,["hex_buffer_serde::var_len::HexForm"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1060]}
